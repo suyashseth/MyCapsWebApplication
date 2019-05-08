ab9a76f9426f45974233a62fa8f67eab59462ddb
QSP <QSP@192.168.0.13>
2019-05-08 16:49:48 +0530

    First Commit

---
+++ src/com/caps/basics/DateServlet.java
@@ -0,0 +1,25 @@
+package com.caps.basics;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/date")
+public class DateServlet extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req,
+	HttpServletResponse resp) throws ServletException, IOException {
+		
+		resp.setContentType("text/html");
+		PrintWriter out = resp.getWriter();
+		out.print("<h1>"+new Date()+"</h1>");
+				
+	}
+}
